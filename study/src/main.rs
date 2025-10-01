0a8a42610b4e2e102e32a4dcb225ee7c48f29965
Moyuin-aka <ytmfsssu@gmail.com>
2025-10-02 02:57:45 +0800

    准备写写简单的小题目了

--- study/src/main.rs
+++ study/src/main.rs
@@ -1,15 +1,16 @@
 
-use std::io;
-fn main() {
-    let mut input =String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read number");
 
-    let number:i32 = input.trim().parse().expect("Failed to read number");
-    if number % 3 == 0 {
-        println!("the number {} is OK", number);
-    } else if number % 2 == 0 {
-        println!("the number {} is OK", number);
-    } else {
-        println!("the number {} is No", number);
+fn greet(){
+    let _zh_tw="你好，世界！";
+    let zh="你好，世界！";
+    let en="Hello,World!";
+    let fr ="Bonjour,le monde!";
+    let regions=[fr,en,zh];
+    //for region in regions.iter(){
+    for region in regions{
+        println!("{}",region);
     }
+}
+fn main() {
+    greet();
 }
